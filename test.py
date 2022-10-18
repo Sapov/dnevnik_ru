f8f43d934826db99e941cdd4f133ca8b1fc611dc
Alexander <39631760+Sapov@users.noreply.github.com>
2022-10-18 19:28:16 +0300

    Delete test.py

--- test.py
+++
@@ -1,3 +0,0 @@
-import  pywhatkit
-pywhatkit.sendwhatmsg('+79802423868', 'Привет мир!', 0, 16, 15, True, 2)
-# pywhatkit.sendwhatmsg_to_group("HlypFhbZrJKK8gtiVgnf1c", "Новый тест", 0, 15, 15, True, 2)
