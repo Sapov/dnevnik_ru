f275a8b8e73fdc8f6b96a55922569e29bff713a6
Sapov <Sapov@mail.ru>
2024-02-01 11:41:38 +0300

    _

--- main.py
+++ main.py
@@ -1,57 +1,60 @@
+import pywhatkit
 from pydnevnikruapi.dnevnik import dnevnik
 from datetime import datetime, time
-import data_pass
+import data_pass_for_prod
 import schedule
 from send_mail import send_mail
 
 from time import sleep
 
 
-# Получаем доступ через логин и пароль
-def print_homework(login, password):
-    dn = dnevnik.DiaryAPI(login, password)
-    curent_datetime = datetime.now()
-    a = dn.get_school_homework(data_pass.idschool,
-                               datetime(curent_datetime.year, curent_datetime.month, curent_datetime.day + 1),
-                               datetime(curent_datetime.year, curent_datetime.month, curent_datetime.day + 1))
-    lst_homework = []
-    for i in a['works']:
-        lst_homework.append(f'{"=" * 4} Предмет: {data_pass.items[i["subjectId"]]} {"=" * 4}\nЗадание:{i["text"]}\n')
-    return lst_homework
+class HomeWork:
+    def __init__(self, login, password):
+        self.password = password
+        self.login = login
+
+    # Получаем доступ через логин и пароль
+    def print_homework(self, login, password):
+        """получаем домашку на следующий день"""
+        dn = dnevnik.DiaryAPI(login, password)
+        current_datetime = datetime.now()
+        a = dn.get_school_homework(data_pass_for_prod.idschool,
+                                   datetime(current_datetime.year, current_datetime.month, current_datetime.day + 1),
+                                   datetime(current_datetime.year, current_datetime.month, current_datetime.day + 1))
+        lst_homework = []
+        for i in a['works']:
+            lst_homework.append(
+                f'{"=" * 4} Предмет: {data_pass_for_prod.items[i["subjectId"]]} {"=" * 4}\nЗадание:{i["text"]}\n')
+        return lst_homework
+
+    def main(self):
+        today_ = datetime.today()
+        time_format = today_.strftime('%d/%m/%y')
+
+        lst = self.print_homework(login=data_pass_for_prod.login, password=data_pass_for_prod.password)
+        text = str(f'{"-" * 6} Дата: {time_format} {"-" * 6}\n\n')
+        for i in range(len(lst)):
+            text += lst[i] + " \n"
+        print(text)
+
+        # send_mail(text, f'Илюшкина домашка на {time_format}', dest_email=data_pass_for_prod.mail_mather)
+        send_mail(text, f'Илюшкина домашка на {time_format}', dest_email=data_pass_for_prod.mail_phather)
+
+        # pywhatkit.sendwhatmsg('+79531193367', text, 11, 40, 1, True, 2)
 
 
-def main():
-    today_ = datetime.today()
-    print(today_)
-    print(type(today_))
-    time_format = today_.strftime('%d/%m/%y')
-
-    lst = print_homework(login=data_pass.login, password=data_pass.password)
-    text = str(f'{"-" * 6} Дата: {time_format} {"-" * 6}\n\n')
-    for i in range(len(lst)):
-        text += lst[i] + " \n"
-    print(text)
-
-    send_mail(text, f'Илюшкина домашка на {time_format}', dest_email=data_pass.mail_mather)
-    send_mail(text, f'Илюшкина домашка на {time_format}', dest_email=data_pass.mail_phather)
-
-
-    # pywhatkit.sendwhatmsg('data.tel', text, 0, 53, 15, True, 2)
-
-
-#    pywhatkit.sendwhatmsg(data_pass.tel, text, curent_datetime.hour, curent_datetime.minute + 1, 15, True, 2)
+# pywhatkit.sendwhatmsg(data_pass_for_prod.tel, text, current_datetime.hour, current_datetime.minute + 1, 15, True, 2)
 
 
 if __name__ == '__main__':
-    main()
+    hm = HomeWork(data_pass_for_prod.login, data_pass_for_prod.password)
 
-    # schedule.every().day.at("14:00").do(main)
-    schedule.every().monday.at("11:20").do(main)
-    schedule.every().tuesday.at("11:10").do(main)
-    schedule.every().wednesday.at("11:00").do(main)
-    schedule.every().thursday.at("11:25").do(main)
+    schedule.every().monday.at("21:12").do(hm.main)
+    schedule.every().tuesday.at("11:10").do(hm.main)
+    schedule.every().wednesday.at("11:00").do(hm.main)
+    schedule.every().thursday.at("11:25").do(hm.main)
     #   schedule.every().friday.at("19:00").do(main)
-    schedule.every().sunday.at("11:00").do(main)
+    schedule.every().sunday.at("11:00").do(hm.main)
     while True:
         schedule.run_pending()
         sleep(1)
